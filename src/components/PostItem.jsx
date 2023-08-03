a40691d776de6a16dfa78f3a29eb47878c3f570a
Serhii Neliubov <84928517+Serhii-Neliubov@users.noreply.github.com>
2023-08-03 12:11:59 +0300

    Update PostItem.jsx

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -9,7 +9,7 @@ const PostItem = ({ post, number, removePost }) => {
     <div className="post">
       <div className="post__content">
         <strong>
-          {post.id}. {post.title}
+          {number}. {post.title}
         </strong>
         <div>{post.body}</div>
       </div>
